e53668c970c6a65590e264f4f69aa8e42b2e9f1c
Diego Huerfano <diego@pop-os.localdomain>
2023-03-05 19:55:39 +0100

    implementation of frustum logic, evaluation with random harmonic coefficients. Test passing

--- frustum_branch.py
+++ frustum_branch.py
@@ -1,18 +1,91 @@
+from typing import Tuple
 import numpy as np
 
-
-xdim, ydim, zdim = 10, 10, 10
-dx, dy, dz = 1, 1, 1
-nb_rays = 5
-nb_samples = 7
-nb_cameras = 6
-cameras = np.random.rand(nb_cameras, 6) # position and orientation
-
-def rays_to_frustrum(rays, nb_samples):
-    return 0
+from spherical_harmonics import sh_spherical
 
 
-def frustrum_to_harmonics(samples):
-    return 0
+# relative 8 ijk-neighbours:
+delta_ijk = np.array([(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1),
+                      (1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 1, 1)]).reshape((8, 3))
 
 
+def rays_to_frustrum(origins: np.ndarray, dirs: np.ndarray, samples: np.ndarray,
+                     delta_voxel: np.array = np.array([1, 1, 1])) -> np.ndarray:
+    """Computes the bottom-left-closest voxel (integer coordinates) of for the given rays and samples.
+
+    A ray is defined by its origin (x0, y0, z0) and its direction (theta, phi)
+         with 0 <= theta <= pi and 0 <= phi < 2*pi
+
+    A sample along a ray is defined by its distance s to the ray origin:
+           (x0, y0, z0) + s * (sin(theta)cos(phi), sin(theta)sin(phi), cos(theta))
+
+    Args: 
+        origins (np.ndarray): of shape (nb_rays, 3), second dim gives xyz
+        dirs (np.ndarray): of shape (nb_rays, 2), second dim gives (theta, phi)
+        samples (np.ndarray): of shape (nb_rays, nb_samples), second dim gives the distance samples
+                 for the ray as defined above
+        delta_voxel (np.array): of length 3, defining (dx, dy, dz) for voxels. Default (1, 1, 1)
+
+    Returns: np.ndarray of shape (nb_rays, nb_samples, 8, 3), forth dim gives the integer 3d-coordinates ijk
+                    of 8 neightbours of bottom-left-closest voxel containing resp. sample
+    """
+
+    n_rays = origins.shape[0]
+    assert n_rays == dirs.shape[0]
+    assert n_rays == samples.shape[0]
+
+    samples = np.expand_dims(samples, axis=2)
+    dir_vecs = np.expand_dims(np.stack([np.sin(dirs[:,0]) * np.cos(dirs[:,1]),
+                                        np.sin(dirs[:,0]) * np.sin(dirs[:,1]),
+                                        np.cos(dirs[:,0])], axis=1),
+                              axis=1)
+    origins = np.expand_dims(origins, axis=1)
+    sample_points = origins + samples * dir_vecs
+
+    # get ijk coordinates according to (dx, dy, dz):
+    frustum = np.floor(sample_points / delta_voxel).astype(int)
+
+    # compute integer index coords of 8 neighbours:
+    frustum = np.expand_dims(frustum, axis=2)
+    frustum = frustum + delta_ijk
+
+    return frustum
+
+
+def frustrum_to_harmonics(frustum: np.ndarray, ray_dirs: np.ndarray,
+                          grid: np.ndarray, opacity: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """computes the multiplication of harmonics (evaluated at a fixed direction per ray and the model 
+    coefficients of the 8 integer neighbours defined by frustum per sample.
+    
+    Args: 
+        frustum (np.ndarray): of shape (nb_rays, nb_samples, 8, 3), given by rays_to_frustrum function
+        ray_dirs (np.ndarray): of 2 shape(nb_rays, 2), second dim gives (theta, phi)
+        grid (np.ndarray): of shape 9 (xdim, ydim zdim, 3, 9) giving model 9 harmonic coeficients per color channel
+        opacity (np.ndarray): of shape 9 (xdim, ydim zdim) giving model opacity
+        
+    Returns: Tuple[np.ndarray, np.ndarray] resp. neigh_harmonics and neigh_densities. First array is of shape
+    (nb_rays, nb_samples, 8, 3, 9) and gives the evaluated 9 harmonics weigthed by model coefficients for each
+    RGB-channel and 8 neighbours of each sample of each ray. Second array is of shape (nb_rays, nb_samples, 8) and
+    gives the model opacities at all 8 neighbours of each sample of each ray.
+    """
+    
+    assert frustum.shape[0] == ray_dirs.shape[0]
+    
+    # retrieve model coefficients of harmonics at 8 neigbour indexes:
+    neigh_harmonics_coeff = grid[tuple(frustum.T)]
+    # resulting array of shape nb_rays x nb_samples x 8 x 3 x 9:
+    neigh_harmonics_coeff = np.moveaxis(neigh_harmonics_coeff, (0, 1, 2), (2, 1, 0))
+
+    # retrieve model density at 8 neigbour indexes:
+    neigh_densities = opacity[tuple(frustum.T)]
+    neigh_densities = np.moveaxis(neigh_densities, (0, 1, 2), (2, 1, 0)) # nb_rays x nb_samples x 8
+
+    # evaluations of 8 neighbours harmonics are done in the same ray direction (parallel to it):
+    sh = sh_spherical(ray_dirs[:,0], ray_dirs[:,1]) # nb_rays x 9
+    sh = np.expand_dims(sh, axis=(2, 3, 4))
+    sh = np.moveaxis(sh, 1, 4) # nb_rays x 1 x 1 x 1 x 9
+    
+    # weigh harmonics with model grid coefficients at all 8 neighbours
+    neigh_harmonics = neigh_harmonics_coeff * sh 
+
+    return neigh_harmonics, neigh_densities
--- test_frustum_branch.py
+++ test_frustum_branch.py
@@ -1,29 +1,69 @@
 import numpy as np
 import unittest
 from scipy.special import *
-# from renderer import *
 from frustum_branch import *
 
 
 class TestArchitecture(unittest.TestCase):
     def setUp(self):
-        self.idim = 5
-        self.diff = 1.0
-        # So the grid goes like [0.0, 1.0, ... , 4.0]
+        self.idim = 100
+        # So the grid looks like [0.0, 1.0, ... , 100.0]^3, functions take (dx,dy,dz)=(1,1,1) by default
         self.nb_rays = 7
         self.nb_samples = 11
+        # 9 harmonic coeficients per color channel, 3 channels
+        self.grid = np.random.rand(self.idim, self.idim, self.idim, 3, 9)
+        self.opacity = np.random.rand(self.idim, self.idim, self.idim)
 
-    def test_rays_to_frustrum(self):
-        R = np.random.rand(self.nb_rays, 6)
-        grid = np.random.rand([self.idim, self.idim, self.idim, 10]) # first one for occlusion, between [0,1] the rest for harmonics
-        F = rays_to_frustrum(R, grid)
-        np.testing.assert_array_almost_equal(F.shape, np.array([self.nb_rays, self.idim, self.idim, self.idim, 3]))
+    def test_rays_to_frustrum_shape(self):
+        # shifts/scalings are just for testing and making sure that random tesing samples does not overflow grid:
+        ray_origins = np.random.rand(self.nb_rays, 3) * 20 + self.idim/2
+        ray_dirs = np.random.rand(self.nb_rays, 2) * np.array([np.pi, 2*np.pi]) # (theta, phi)
+        ray_samples = np.random.rand(self.nb_rays, self.nb_samples) * 30
+        frustum = rays_to_frustrum(ray_origins, ray_dirs, ray_samples)
+        np.testing.assert_array_almost_equal(frustum.shape,
+                                             np.array([self.nb_rays, self.nb_samples, 8, 3]))
 
-    def test_frustrum_to_harmonics(self):
-        R = np.random.rand(self.nb_rays, 6)
-        grid = np.random.rand([self.idim, self.idim, self.idim, 10]) # first one for occlusion, between [0,1] the rest for harmonics
-        H = frustrum_to_harmonics(R, grid)
-        np.testing.assert_array_almost_equal(H.shape, np.array([self.nb_rays, self.idim, self.idim, self.idim, 9]))
+    def test_frustrum_to_harmonics_shape(self):
+        # shifts/scalings are just for testing and making sure that random tesing samples does not overflow grid:
+        ray_origins = np.random.rand(self.nb_rays, 3) * 20 + self.idim/2
+        ray_dirs = np.random.rand(self.nb_rays, 2) * np.array([np.pi, 2*np.pi]) # (theta, phi)
+        ray_samples = np.random.rand(self.nb_rays, self.nb_samples) * 30
+        frustum = rays_to_frustrum(ray_origins, ray_dirs, ray_samples)
+        neighs_harmonics, neighs_opacities = frustrum_to_harmonics(frustum, ray_dirs, self.grid, self.opacity)
+        np.testing.assert_array_almost_equal(neighs_harmonics.shape,
+                                             np.array([self.nb_rays, self.nb_samples, 8, 3, 9]))
+        np.testing.assert_array_almost_equal(neighs_opacities.shape,
+                                             np.array([self.nb_rays, self.nb_samples, 8]))
+
+    def test_rays_to_frustum_easy_values(self):
+        nr_rays = 4
+        nr_samples = 3
+        root3 = np.sqrt(3)
+        ray_origins = np.ones((nr_rays, 3)) * 50
+        ray_dirs = np.array([[np.pi/2, 0], # pointing to +x
+                             [np.pi/2, np.pi/2], # pointing to +y
+                             [0, 0], # pointing to +z
+                             [0.95531661812451, np.pi/4]] # pointing to (1,1,1)
+                           ).reshape((nr_rays, 2))
+        # samples defined by following distances from ray origin along ray dir:
+        ray_samples = np.stack([np.array([2.5, 5.7, 8.5]),
+                                np.array([3.3, 4.9, 9.2]),
+                                np.array([3.5, 5.7, 7.5]),
+                                np.array([1.5*root3, 2.2*root3, 5.8*root3])]
+                              ).reshape((nr_rays, nr_samples))
+
+        # test only first neighbour (relative (0, 0, 0)-neighbour)
+        voxels_ijk = np.array([
+                                [[52, 50, 50], [55, 50, 50], [58, 50, 50]],
+                                [[50, 53, 50], [50, 54, 50], [50, 59, 50]],
+                                [[50, 50, 53], [50, 50, 55], [50, 50, 57]],
+                                [[51, 51, 51], [52, 52, 52], [55, 55, 55]]
+                              ])
+
+        assert voxels_ijk.shape == (nr_rays, nr_samples, 3)
+        frustum = rays_to_frustrum(ray_origins, ray_dirs, ray_samples)
+        np.testing.assert_array_almost_equal(frustum[:, :, 0, :],
+                                             voxels_ijk)
 
 
 if __name__=='__main__':
