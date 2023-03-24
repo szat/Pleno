301e67413a682bd36378dc3e8e21581d3c7246f1
szat <artabalt@gmail.com>
2023-03-24 13:13:49 +0100

    getting and drawing the rays is working

--- render.py
+++ render.py
@@ -120,144 +120,54 @@ cam_x = get_arrow(np.zeros(3)+z, x+z, [1, 0, 0]) #red
 cam_y = get_arrow(np.zeros(3)+z, y+z, [0, 1, 0]) #green
 o3d.visualization.draw_geometries([box, mesh, coord_obj, coord_w, cam_z, cam_x, cam_y])
 
-F = 500
-width = 640
-height = 480
-px = 1
-py = 1
-fx = F/px
-fy = F/py
-cx = width/2
-cy = height/2
-skew = 0
-intrinsic = np.array([[F/px, skew, cx],[0, F/py, cy], [0, 0, 1]])
-cam = o3d.geometry.LineSet.create_camera_visualization(width, height, intrinsic, np.eye(4), scale=10.0)
-o3d.visualization.draw_geometries([box, mesh, coord_obj, coord_w, cam_z, cam_x, cam_y, cam])
+nx, ny = (4, 3)
+tics_x = np.expand_dims(np.linspace(-1, 1, nx), 1)
+tics_y = np.expand_dims(np.linspace(-1, 1, ny), 1)
 
-vec = coord_obj.get_center() - cam.get_center()
-vec = vec/np.linalg.norm(vec)
-R = rotation_align(np.array([0, 0, 1]), vec)
-cam.rotate(R, center=coord_w.get_center())
-o3d.visualization.draw_geometries([box, mesh, coord_obj, coord_w, cam_z, cam_x, cam_y, cam])
+xx = tics_x * x
+yy = tics_y * y
 
+xx = np.expand_dims(xx, 0)
+yy = np.expand_dims(yy, 1)
 
+rays = xx + yy
+zz = np.expand_dims(z, [0, 1])
+rays = rays + zz
 
+r_list = []
+for i in range(rays.shape[0]):
+    for j in range(rays.shape[1]):
+        arrow = get_arrow(np.zeros(3), rays[i, j, :])  # green
+        r_list.append(arrow)
 
+scene = o3d.visualization.draw_geometries([box, mesh, coord_obj, coord_w, cam_z, cam_x, cam_y] + r_list)
 
 
 
 
 
 
-# # https://de.mathworks.com/help/vision/ug/camera-calibration.html
-# # https://vita-group.github.io/Fall22/Lecture%2010+11+12.pdf
-# # Maybe this
-# # https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html
-# # https://www.3dflow.net/elementsCV/S3.xhtml
-#
-
-F = 500
-width = 640
-height = 480
-px = 1
-py = 1
-fx = F/px
-fy = F/py
-cx = width/2
-cy = height/2
-skew = 0
-intrinsic = np.array([[F/px, skew, cx],[0, F/py, cy], [0, 0, 1]])
-cam = o3d.geometry.LineSet.create_camera_visualization(width, height, intrinsic, np.eye(4), scale=10.0)
-coord_cam = o3d.geometry.TriangleMesh.create_coordinate_frame()
-coord_cam.scale(10, center=coord_w.get_center())
-#
-obj_center = mesh.get_center()
-coord_obj = o3d.geometry.TriangleMesh.create_coordinate_frame()
-coord_obj = copy.deepcopy(coord_obj).translate(mesh.get_center())
-coord_obj.scale(20, center=coord_obj.get_center())
-
-o3d.visualization.draw_geometries([box, mesh, coord_obj, cam, coord_cam])
 
 
 
+# F = 500
+# width = 640
+# height = 480
+# px = 1
+# py = 1
+# fx = F/px
+# fy = F/py
+# cx = width/2
+# cy = height/2
+# skew = 0
+# intrinsic = np.array([[F/px, skew, cx],[0, F/py, cy], [0, 0, 1]])
+# # intrinsic = np.array([[1, skew, cx],[0, 1, cy], [0, 0, 1]])
+# cam = o3d.geometry.LineSet.create_camera_visualization(width, height, intrinsic, np.eye(4), scale=10.0)
+# o3d.visualization.draw_geometries([box, mesh, coord_obj, coord_w, cam_z, cam_x, cam_y, cam])
 
-
-#
-# vec = coord_obj.get_center() - coord_cam.get_center()
+# vec = coord_obj.get_center() - cam.get_center()
 # vec = vec/np.linalg.norm(vec)
-# R = rotation_align(vec, np.array([0, 0, 1]))
-#
-# coord_cam2 = copy.deepcopy(coord_cam)
-# coord_cam2.rotate(R, center=coord_cam.get_center())
-#
-# cam2 = copy.deepcopy(cam)
-# cam2.rotate(R, center=coord_cam.get_center())
-#
-# # o3d.visualization.draw_geometries([box, mesh, coord_obj, coord_cam2, cam2])
-#
-# Camera
-position = cam.get_center()
-direction = np.array([0, 0, 1])
-scale = 10
-cone_height = scale * 0.2
-cylinder_height = scale * 0.8
-cone_radius = scale / 50
-cylinder_radius = scale / 50
-arrow_F = o3d.geometry.TriangleMesh.create_arrow(cone_radius=1,
-                                                    cone_height=cone_height,
-                                                    cylinder_radius=0.5,
-                                                    cylinder_height=cylinder_height)
-arrow_F.compute_vertex_normals()
-o3d.visualization.draw_geometries([box, mesh, coord_obj, coord_cam, cam, arrow_F])
+# R = rotation_align(np.array([0, 0, 1]), vec)
+# cam.rotate(R, center=coord_w.get_center())
+# o3d.visualization.draw_geometries([box, mesh, coord_obj, coord_w, cam_z, cam_x, cam_y, cam])
 
-R_around_x = mesh.get_rotation_matrix_from_xyz((-np.pi/2, 0, 0))
-R_around_y = mesh.get_rotation_matrix_from_xyz((0, np.pi/2, 0))
-
-Rx = rotation_align(np.array([0, 0, 1]), np.array([0, 1, 0]))
-np.testing.assert_allclose(Rx, R_around_x, atol=1e-10)
-
-Ry = rotation_align(np.array([0, 0, 1]), np.array([1, 0, 0]))
-np.testing.assert_allclose(Ry, R_around_y, atol=1e-10)
-
-
-Rx * np.array([0, 0, 1])
-
-
-arrow_fx = copy.deepcopy(arrow_F)
-arrow_fy = copy.deepcopy(arrow_F)
-arrow_fx.rotate(R_around_y, center=np.zeros(3))
-arrow_fy.rotate(R_around_x, center=np.zeros(3))
-arrow_fx.scale(0.640, center=np.zeros(3))
-arrow_fy.scale(0.480, center=np.zeros(3))
-arrow_fy.translate((0, 0, 1*scale))
-arrow_fx.translate((0, 0, 1*scale))
-
-o3d.visualization.draw_geometries([box, mesh, coord_obj, coord_cam, cam, arrow_F, arrow_fy, arrow_fx])
-
-arrow_x = get_arrow(np.array([0, 0, 0]), np.array([10, 0, 0]))
-arrow_y = get_arrow(np.array([0, 0, 0]), np.array([0, 10, 0]))
-arrow_z = get_arrow(np.array([0, 0, 0]), np.array([0, 0, 10]))
-arrow_d = get_arrow(np.array([10, 10, 10]), np.array([20, 20, 20]))
-o3d.visualization.draw_geometries([box, mesh, coord_w, arrow_x, arrow_y, arrow_z, arrow_d])
-
-# arrow_fx_R = copy.deepcopy(arrow_fx)
-# arrow_fy_R = copy.deepcopy(arrow_fy)
-# arrow_F_R = copy.deepcopy(arrow_F)
-# cam_R = copy.deepcopy(cam)
-# coord_cam_R = copy.deepcopy(coord_cam)
-#
-#
-# arrow_fx_R.rotate(R, center=np.zeros(3))
-# arrow_fy_R.rotate(R, center=np.zeros(3))
-# arrow_F_R.rotate(R, center=np.zeros(3))
-# cam_R.rotate(R, center=np.zeros(3))
-# coord_cam_R.rotate(R, center=np.zeros(3))
-#
-# # o3d.visualization.draw_geometries([box, mesh, coord_obj, coord_cam, cam_R, arrow_F_R, arrow_fy_R, arrow_fx_R])
-#
-# # Position: the location of the camera in 3D space
-# # Orientation: the direction that the camera is pointing in 3D space
-# # Field of view: the angle of the cone of vision that the camera captures
-# # Aspect ratio: the ratio of the width to the height of the image plane
-# # Near and far planes: the distances from the camera at which objects are visible
-#
